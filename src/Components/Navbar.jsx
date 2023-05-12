bfed6074a750f303c2cee3c8f2c2347f3ce616c0
riyaalexander <riyaalexander@Riyas-MacBook-Air.local>
2023-05-12 18:11:08 -0400

    index.html

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import SearchBar from "./SearchBar";
-
-import FormPage from "./Form";
+import { Link } from "react-router-dom";
+import "../Styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      {FormPage}
-      
-      <div className="search-bar-container">
-        <SearchBar />
+    <div className="navbar">
+      <h1 className="navbar-title">Board, Never Bored!</h1>
+      <div className="navbar-links">
+        <input type="text" placeholder="Search games" className="search-input" />
+        <select className="sort-select">
+          <option value="asc">Sort A-Z</option>
+          <option value="desc">Sort Z-A</option>
+        </select>
+        <Link to="/form" className="form-link">Add Game</Link>
       </div>
     </div>
   );
